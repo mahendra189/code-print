1893a2d7a8c9a7beb2b176278df22469f8e9d89c
Mahendra <160317468+mahendra189@users.noreply.github.com>
2024-09-04 10:42:50 +0000

    Setup Changes

--- code-print/src/app/page.tsx
+++ code-print/src/app/page.tsx
@@ -1,8 +1,12 @@
-import Image from "next/image";
+import Stack from '@mui/joy/Stack';
+
+import Editor from './editor';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 wallpaper-background">
+      <h1 className='font-bold'>Code Print</h1>
+      <Editor />
 
     </main>
   );
